8626e9efdd5b8b0ff2dfcbeab9a631228ec027fb
Muriloaoi <muriloferreira547@gmail.com>
2025-02-02 15:39:18 -0300

    0202

--- scripts/arado.gd
+++ scripts/arado.gd
@@ -3,9 +3,11 @@ extends Area2D
 var area_arada = "nao"
 var plantado = false
 var cresceu = "nao"
+@onready var tomate = load("res://tomato.tscn").instantiate().duplicate()
 
 func _ready():
-	$tomate2/anim_tomato.hide()
+	add_child(tomate)
+	get_node("tomato").get_node("tomate/anim_tomato").animation_finished.connect(_on_anim_tomato_animation_finished)
 	pass
 
 func _process(delta):
@@ -15,15 +17,16 @@ func _process(delta):
 	if area_arada == "sim" and Dados.sementes_de_tomate >= 1:
 		if Input.is_action_just_pressed("interacao") and not plantado:
 			
+			
 			Dados.sementes_de_tomate -= 1
-			$tomate2/anim_tomato.show()  # Mostrar o nodo do tomate
-			$tomate2/anim_tomato.play("tomate_crescendo")  # Começar a animação do tomate (ajuste o nome da animação conforme necessário)
+			get_node("tomato").visible = true  # Mostrar o nodo do tomate
+			get_node("tomato").get_node("tomate/anim_tomato").play("tomate_crescendo")  # Começar a animação do tomate (ajuste o nome da animação conforme necessário)
 			plantado = true
 	
 	if cresceu == "sim":
 		if area_arada == "sim" and Input.is_action_just_pressed("acao"):
-			$tomate2/anim_tomato.hide()
-			$tomate2/anim_tomato.stop()
+			get_node("tomato").visible = false
+			get_node("tomato").get_node("tomate/anim_tomato").stop()
 			cresceu = "nao"
 			plantado = false
 			
--- scripts/dados.gd
+++ scripts/dados.gd
@@ -12,6 +12,5 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	#print(sementes_de_tomate)
-	print("que demora")
 	pass
 	
