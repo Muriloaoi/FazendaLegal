fce5f632c8f4e13a4e848ce3f0bb36f2a3b78d3e
Muriloaoi <muriloferreira547@gmail.com>
2025-02-02 00:06:39 -0300

    Comentario2

--- scripts/dados.gd
+++ scripts/dados.gd
@@ -11,6 +11,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print(sementes_de_tomate)
+	#print(sementes_de_tomate)
 	pass
 	
